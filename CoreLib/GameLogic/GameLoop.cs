581b4468c65610f7008f7630309b6a3c09dfe1cc
Hamiller78 <Kneesch@gmx.de>
2021-07-11 20:30:51 +0200

    Add porject setup

---
+++ CoreLib/GameLogic/GameLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoreLib.GameLogic
+{
+    public class GameLoop
+    {
+        private readonly IServiceProvider _gameServices;
+
+        public GameLoop(IServiceProvider gameServices) =>
+            _gameServices = gameServices;
+
+        public void ProcessGameTick(long tickNumber)
+        {
+
+        }
+    }
+}
---
+++ CoreUnitTests/CoreUnitTests/GameLogicTests/GameLoopTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace CoreUnitTests
+{
+    public class GameLoopTests
+    {
+        [Fact]
+        public void NormalRun_ProcessGameTick_ExpectedBehaviour()
+        {
+
+        }
+
+        }
+    }
+}
